861c1a0dc53a49133e49191a7a9cee7a59e2b172
Astaroth <astaroth.txs66@gmail.com>
2022-05-06 01:53:19 -0500

    mejora fomulario

--- resources/js/script.js
+++ resources/js/script.js
@@ -59,8 +59,11 @@ function insertRowInTransactionTable(transactionObj) {
 
     let newDeleteCell = newTransactionRowRef.insertCell(4);
     let deleteButton = document.createElement("button");
-    deleteButton.textContent = "Eliminar"
+    deleteButton.textContent = "X"
+    deleteButton.style.color = "#FFFFFF"
+    deleteButton.style.background = "#ff0000"
     newDeleteCell.appendChild(deleteButton)
+
 
     deleteButton.addEventListener("click", (event) => {
         let TransactionRow = event.target.parentNode.parentNode;
@@ -91,4 +94,28 @@ function saveTransactionObj(transactionObj) {
     let transactionArrayJSON = JSON.stringify(myTransactionArray);
     //guardo mi array de trasaccion en el local storage
     localStorage.setItem("transactionData", transactionArrayJSON);
+}
+
+const bdark = document.querySelector('#bdark');
+const body = document.querySelector('body');
+
+load();
+
+bdark.addEventListener('click', e => {
+    body.classList.toggle('darkmode');
+    store(body.classList.contains('darkmode'));
+})
+
+function load() {
+    const darkmode = localStorage.getItem('darkmode');
+
+    if (!darkmode) {
+        store('false');
+    } else if (darkmode == 'true') {
+        body.classList.add('darkmode')
+    }
+}
+
+function store(value) {
+    localStorage.setItem('darkmode', value)
 }
